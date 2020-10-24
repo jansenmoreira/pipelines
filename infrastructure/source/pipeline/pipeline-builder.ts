5ffdf09904090ae47180168737793fd9735e1757
Jansen Moreira <jansenhns@gmail.com>
2020-10-24 01:13:44 -0300

    Fix pipeline artifact output

--- infrastructure/source/pipeline/pipeline-builder.ts
+++ infrastructure/source/pipeline/pipeline-builder.ts
@@ -25,7 +25,7 @@ export class PipelineBuilder extends PipelineProject {
                 artifacts: {
                     'base-directory': 'infrastructure/cdk.out',
                     files: [
-                        'LambdaStack.template.json'
+                        'ApplicationStack.template.json'
                     ]
                 }
             }),
