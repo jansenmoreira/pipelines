7ee20d352bb5f1ee8f3a923322f732b07d7a2b1f
Jansen Moreira <jansenhns@gmail.com>
2020-11-04 03:20:44 -0300

    Removing Bucket

--- infrastructure/source/client/client-stack.ts
+++ infrastructure/source/client/client-stack.ts
@@ -4,12 +4,5 @@ import { Construct, Stack, StackProps } from '@aws-cdk/core';
 export class ClientStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
-        
-        const bucket = new Bucket(this, `${id}ContentBucket`, {
-            publicReadAccess: true,
-            websiteIndexDocument: "index.html",
-            websiteErrorDocument: "error.html",
-            bucketName: 'jansenmoreira.com.br'
-        });
     }
 }
